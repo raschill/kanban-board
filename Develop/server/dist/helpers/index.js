e2ee99f2fa3562dc9f7c9e2123bd025a232c0bbb
raschill <rebeccaaschilling@gmail.com>
2024-12-17 18:21:38 -0600

    file restructuring

--- Develop/server/dist/helpers/index.js
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-export const makeNewToken = (username) => {
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-    return jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-};
